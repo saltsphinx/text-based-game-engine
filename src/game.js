367157ad9042753e6facc5546728abc642aa8849
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2023-05-25 19:41:38 -0500

    Remove closure use, use 'this' keyword instead

--- src/game.js
+++ src/game.js
@@ -1,38 +1,26 @@
 function Game() {
-  const data = {};
-  const scenes = {};
-  const defaultCommands = {};
-  let commands = {};
-  
-  const callCommand = (alias, ...parameters) => {
-    const command = commands[alias];
+  const callCommand = function(alias, ...parameters) {
+    const command = this.commands[alias];
     if (!command) return;
 
-    command(data, ...parameters);
+    command(this.data, ...parameters);
   };
 
-  const initializeScene = (sceneName) => {
-    const scene = scenes[sceneName];
+  const initializeScene = function(sceneName) {
+    const scene = this.scenes[sceneName];
     if (!scene) return;
 
-    removeProps(commands);
-    Object.assign(commands, scene.commands, defaultCommands);
+    this.commands = {...scene.commands, ...this.defaultCommands};
   };
 
   return {
-    data,
-    scenes,
-    defaultCommands,
-    commands,
+    data: {},
+    scenes: {},
+    defaultCommands: {},
+    commands: {},
     callCommand,
     initializeScene,
   };
-}
-
-function removeProps(obj) {
-  for (let propName in obj) {
-    delete obj[propName];
-  }
 }
 
 module.exports = Game;
--- src/scene.js
+++ src/scene.js
@@ -1,15 +1,14 @@
 function Scene() {
-  const commands = {};
   const addCommand = function(cb, ...aliases) {
     aliases.forEach((alias) => {
-      if (commands[alias]) throw new Error('Alias already exists');
+      if (this.commands[alias]) throw new Error('Alias already exists');
 
-      commands[alias] = cb;
+      this.commands[alias] = cb;
     })
   };
 
   return {
-    get commands() { return { ...commands } },
+    commands: {},
     addCommand,
   };
 }
